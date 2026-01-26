d086fe6363d98d67dfd8ff0bd3edc20727d35552
quanvo <vobaquan0147@gmail.com>
2026-01-26 18:03:10 +0700

    fix vite config

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Invite from './components/invite/Invite'
 
 function App() {
 
+  const repoName = import.meta.env.DEV ? "/" : "/plan-out-date";
+
   return (
     // <Home></Home>
-    <BrowserRouter>
+    <BrowserRouter basename={repoName}>
       <Routes>
-        <Route path="/plan-our-date" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/invite" element={<Invite />} />
       </Routes>
     </BrowserRouter>
