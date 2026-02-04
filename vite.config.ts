eb62275716c7dd32984ffc66a311d9531f8ddac1
quanvo <vobaquan0147@gmail.com>
2026-02-04 10:46:28 +0700

    fix: defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
   plugins: [
     react(),
   ],
-  base: '/plan-our-date/',
+  base: command === 'serve' ? '/' : '/plan-our-date/',
 }))
